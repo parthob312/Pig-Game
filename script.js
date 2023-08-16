5d7de61db43e99d83a8edb45378e92d22b3cc56b
parthob312 <sarothipartho312@gmail.com>
2023-08-16 19:36:40 +0600

    update current score

--- script.js
+++ script.js
@@ -18,20 +18,48 @@ dice.classList.add("hidden");
 const score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 
 rollDice.addEventListener("click", function () {
-  const randomDice = Math.trunc(Math.random() * 6) + 1;
-  dice.src = `dice-${randomDice}.png`;
-  dice.classList.remove("hidden");
-  if (randomDice !== 1) {
-    currentScore += randomDice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+  if (playing) {
+    const randomDice = Math.trunc(Math.random() * 6) + 1;
+    dice.src = `dice-${randomDice}.png`;
+    dice.classList.remove("hidden");
+    if (randomDice !== 1) {
+      currentScore += randomDice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+hold.addEventListener("click", function () {
+  if (playing) {
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+
+    if (score[activePlayer] >= 50) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--active");
+      dice.classList.add("hidden");
+    }
+
+    switchPlayer();
   }
 });
